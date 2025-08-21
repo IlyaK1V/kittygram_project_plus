2f09bcff8cc26b2522c1670595347a4f77d9398e
Ilya Kravchenko <ilyak475@gmail.com>
2025-08-21 15:43:54 +0300

    update project

--- kittygram_plus/urls.py
+++ kittygram_plus/urls.py
@@ -2,12 +2,16 @@ from django.urls import include, path
 
 from rest_framework.routers import DefaultRouter
 
-from cats.views import CatViewSet
+from cats.views import CatViewSet, OwnerViewSet, LightCatViewSet
 
 
 router = DefaultRouter()
 router.register('cats', CatViewSet)
+router.register('owner', OwnerViewSet)
+router.register(r'mycats', LightCatViewSet, basename='lightcat')
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('auth/', include('djoser.urls')),
+    path('auth/', include('djoser.urls.jwt')),
 ]
